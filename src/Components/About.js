5b05ac5552d92ca3e735a095cd9c5fdb18093803
CedricM23 <Cedricpbc@gmail.com>
2025-02-10 20:26:55 -0500

    new updates

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,16 +3,16 @@ import githublogo from '../Images/githublogo.png'
 
 function About() {
   return (
-   <>
-   <div className='About-Section'>
-        <img src={githublogo} className='About-Section-Image' style={{width: '300px'}} alt='user'/>
-        <section className='About-Section-Text'>
+    <>
+        <div className='About-Section'>
+            <img src={githublogo} className='About-Section-Image' style={{width: '300px'}} alt='user'/>
+            <section className='About-Section-Text'>
                 <p>I am a lifelong learner and Software Engineer with foundations in front-end development, programming,
                 and version control. Seeking to leverage coding skills and technical expertise in a collaborative
                 development environment to create impactful and complex front-end HTML and CSS code with cms
                 integration.</p>
-        </section>
-   </div>
+            </section>
+        </div>
    </>
   )
 }
