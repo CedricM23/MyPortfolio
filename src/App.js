3b6999d71a1de7255a8e0e95b5e7d3905ceff3bf
CedricM23 <Cedricpbc@gmail.com>
2025-02-12 11:36:46 -0500

    new updates

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './Components/Header';
 import Stack from './Components/Stack';
 import About from './Components/About'
 import Projects from './Components/Projects';
-import Footer from './Components/Footer';
 
 function App() {
   return (
